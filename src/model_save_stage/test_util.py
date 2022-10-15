14ce4492a2063431eeffff642b593c7c23960228
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-10-14 20:56:09 -0600

    creates fixtures for tests

--- src/model_save_stage/test_util.py
+++ src/model_save_stage/test_util.py
@@ -1,4 +1,26 @@
 from . import util
+import pytest
+
+
+@pytest.fixture
+def env():
+    return util.get_env()
+
+
+@pytest.fixture
+def secret_name():
+    return "test"
+
+
+@pytest.fixture
+def secret_string():
+    return "test"
+
+
+@pytest.fixture
+def create_secret(secret_name, secret_string):
+    response = util.create_secret(secret_name, secret_string)
+    return response
 
 
 def test_get_env():
@@ -6,29 +28,22 @@ def test_get_env():
     assert env["model_repository"] is not None
 
 
-def test_create_secret():
-    secret_string = "test"
-    secret_name = "test"
+def test_create_secret(secret_name, secret_string):
     response = util.create_secret(secret_name, secret_string)
     assert response is not None
     assert response["Name"] is not None
 
 
-def test_get_secret():
-    secret_string = "test"
-    secret_name = "test"
-    util.create_secret(secret_name, secret_string)
+def test_get_secret(secret_name, create_secret, secret_string):
     response = util.get_secret(secret_name)
-    assert secret_name == response
+    assert secret_string == response
 
 
-def test_get_model_repository():
-    env = util.get_env()
+def test_get_model_repository(env):
     model_repository = util.get_model_repository(env)
     assert model_repository is not None
 
 
-def test_get_huggingface_secret_name():
-    env = util.get_env()
+def test_get_huggingface_secret_name(env):
     secret_name = util.get_huggingface_secret_name(env)
     assert secret_name is not None
