8367c2648bd01111d799909f86959ed8a41f19fa
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-10-14 20:14:30 -0600

    rename file

---
+++ src/model_save_stage/upload_huggingface_token.py
@@ -0,0 +1,14 @@
+import typer
+from . import util
+
+
+def main():
+    token = typer.prompt("hugging face api token:")
+    env = util.get_env()
+    secret_name = util.get_huggingface_secret_name(env)
+    description = "Huggingfce api token for model hub"
+    util.create_secret(secret_name, token, description=description)
+
+
+if __name__ == "__main__":
+    typer.run(main)
