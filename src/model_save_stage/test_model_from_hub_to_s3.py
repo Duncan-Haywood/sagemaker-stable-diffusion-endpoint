65c0bd0ad81e3ae105f7f18eec55dd5c52c692bd
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-10-15 20:39:18 -0600

    lint

--- src/model_save_stage/test_model_from_hub_to_s3.py
+++ src/model_save_stage/test_model_from_hub_to_s3.py
@@ -1,4 +1,3 @@
-import py
 from . import model_from_hub_to_s3 as upload
 import pytest
 from . import util
@@ -22,6 +21,7 @@ def huggingface_secret_name(env):
 @pytest.fixture
 def hugging_face_token(huggingface_secret_name):
     return util.get_secret(huggingface_secret_name)
+
 
 @pytest.mark.skip(reason="Large file download into RAM")
 def test_load_model(model_id, hugging_face_token):
