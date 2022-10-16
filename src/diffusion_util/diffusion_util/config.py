6e57744558f4ebc302aaf290680b74a3309a4509
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-10-15 22:39:57 -0600

    switch config to python dict

---
+++ src/diffusion_util/diffusion_util/config.py
@@ -0,0 +1,7 @@
+config = dict(
+    model_repository="CompVis/stable-diffusion-v-1-4",
+    huggingface_token_secret_name="huggingface_api_token",
+    model_bucket_name=None,
+    input_bucket_name=None,
+    output_bucket_name=None,
+)
