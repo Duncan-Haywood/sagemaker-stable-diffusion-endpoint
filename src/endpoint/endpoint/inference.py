3e95e431a29dd302abd51b5cb6e4f12bd494fc25
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-10-18 12:13:22 -0600

    adds gpu support

--- src/endpoint/endpoint/inference.py
+++ src/endpoint/endpoint/inference.py
@@ -21,70 +21,63 @@ accept is the return accept type from the HTTP Request, e.g. application/json.
 
 from diffusers import StableDiffusionInpaintPipeline
 import torch
+from typing import Any, Tuple, Union, Dict
+from diffusion_util import util
+from logging import Logger
+
+logger = Logger(__name__)
 
 
-def model_fn(model_dir):
-    """This overwrites the load() function in a sagemaker HuggingFaceHandlerService. It tells how to load a hugging face model in the HuggingFaceModel __init__ call. For our case, it will load the model from the s3 bucket where this is bundled with the model file (model.tar.gz) for our StableDiffuserInpaintPipeline (SDIP).
-    For the pipeline itself.
+def download_model(local_dir: str):
+    """Downloads model from s3 model bucket to local_dir"""
+    # model_is_in_folder = NotImplemented
+    # if model_is_in_folder:
+    #     logger.info("model already downloaded")
+    # else:
+    config = util.get_config()
+    bucket_name = util.get_model_bucket_name(config)
+    key = util.get_model_s3_key(config)
+    util.download_from_s3(bucket_name, local_dir, key)
+    logger.info("model downloaded from s3")
+
+
+def to_gpu(model: StableDiffusionInpaintPipeline) -> StableDiffusionInpaintPipeline:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    return model
+
+
+def model_fn(model_dir: str) -> StableDiffusionInpaintPipeline:
+    """Load the saved model from the s3 bucket for the StableDiffuserInpaintPipeline (SDIP).
+
     Pipeline for text-guided image inpainting using Stable Diffusion. *This is an experimental feature*.
     This model inherits from [`DiffusionPipeline`]. Check the superclass documentation for the generic methods the
     library implements for all the pipelines (such as downloading or saving, running on a particular device, etc.)
     """
-    model = StableDiffusionInpaintPipeline.from_pretrained(model_dir)
+    download_model(model_dir)
+    model = StableDiffusionInpaintPipeline.from_pretrained(
+        model_dir,
+        revision="fp16",
+        torch_dtype=torch.float16,
+    )
+    logger.info("model loaded")
+    model = to_gpu(model)
+
     return model
 
 
-def predict_fn(prompt, model, **kwargs):
+def predict_fn(
+    args_kwargs_tuple: Tuple[Tuple[Any], Dict[str, Any]],
+    model: StableDiffusionInpaintPipeline,
+) -> Union[dict, tuple]:
     """
-        This overwrites the predict handler. It is responsible for model predictions. Calls the `__call__` method of the provided `modelline`
-        on decoded_input_data deserialized in input_fn. Runs prediction on GPU if is available.
+        This is what is called when sagemaker_endpoint.predict is run.
+        runs predict on decoded_input_data deserialized in input_fn. Runs prediction on GPU if is available.
         Args:
-            prompt (dict): deserialized decoded_input_data returned by the input_fn
-            model : Model returned by the `load` method or if it is a custom module `model_fn`.
-    Function invoked when calling the pipeline for generation.
-    Args + kwargs:
-        prompt (`str` or `List[str]`):
-            The prompt or prompts to guide the image generation.
-        init_image (`torch.FloatTensor` or `PIL.Image.Image`):
-            `Image`, or tensor representing an image batch, that will be used as the starting point for the
-            process. This is the image whose masked region will be inpainted.
-        mask_image (`torch.FloatTensor` or `PIL.Image.Image`):
-            `Image`, or tensor representing an image batch, to mask `init_image`. White pixels in the mask will be
-            replaced by noise and therefore repainted, while black pixels will be preserved. If `mask_image` is a
-            PIL image, it will be converted to a single channel (luminance) before use. If it's a tensor, it should
-            contain one color channel (L) instead of 3, so the expected shape would be `(B, H, W, 1)`.
-        strength (`float`, *optional*, defaults to 0.8):
-            Conceptually, indicates how much to inpaint the masked area. Must be between 0 and 1. When `strength`
-            is 1, the denoising process will be run on the masked area for the full number of iterations specified
-            in `num_inference_steps`. `init_image` will be used as a reference for the masked area, adding more
-            noise to that region the larger the `strength`. If `strength` is 0, no inpainting will occur.
-        num_inference_steps (`int`, *optional*, defaults to 50):
-            The reference number of denoising steps. More denoising steps usually lead to a higher quality image at
-            the expense of slower inference. This parameter will be modulated by `strength`, as explained above.
-        guidance_scale (`float`, *optional*, defaults to 7.5):
-            Guidance scale as defined in [Classifier-Free Diffusion Guidance](https://arxiv.org/abs/2207.12598).
-            `guidance_scale` is defined as `w` of equation 2. of [Imagen
-            Paper](https://arxiv.org/pdf/2205.11487.pdf). Guidance scale is enabled by setting `guidance_scale >
-            1`. Higher guidance scale encourages to generate images that are closely linked to the text `prompt`,
-            usually at the expense of lower image quality.
-        eta (`float`, *optional*, defaults to 0.0):
-            Corresponds to parameter eta (η) in the DDIM paper: https://arxiv.org/abs/2010.02502. Only applies to
-            [`schedulers.DDIMScheduler`], will be ignored for others.
-        generator (`torch.Generator`, *optional*):
-            A [torch generator](https://pytorch.org/docs/stable/generated/torch.Generator.html) to make generation
-            deterministic.
-        output_type (`str`, *optional*, defaults to `"pil"`):
-            The output format of the generate image. Choose between
-            [PIL](https://pillow.readthedocs.io/en/stable/): `PIL.Image.Image` or `np.array`.
-        return_dict (`bool`, *optional*, defaults to `True`):
-            Whether or not to return a [`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] instead of a
-            plain tuple.
-        callback (`Callable`, *optional*):
-            A function that will be called every `callback_steps` steps during inference. The function will be
-            called with the following arguments: `callback(step: int, timestep: int, latents: torch.FloatTensor)`.
-        callback_steps (`int`, *optional*, defaults to 1):
-            The frequency at which the `callback` function will be called. If not specified, the callback will be
-            called at every step.
+            args_kwargs_tuple (tuple(tuple, dict)): deserialized (args, kwargs) as described below - returned by the input_fn. See `StableDiffusionInpaintPipeline` in `diffusers` for args and kwargs.
+            model : Model returned by the `model_fn`.
+
+
     Returns:
         [`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] or `tuple`:
         [`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] if `return_dict` is True, otherwise a `tuple. (default true) type: ordered dict
@@ -92,11 +85,18 @@ def predict_fn(prompt, model, **kwargs):
         list of `bool`s denoting whether the corresponding generated image likely represents "not-safe-for-work"
         (nsfw) content, according to the `safety_checker`.
     """
-    with torch.autocast("cuda"):
-        prediction = model(prompt, kwargs)
+    args = args_kwargs_tuple[0]
+    kwargs = args_kwargs_tuple[1]
+    predictions = model(*args, **kwargs)
+    return predictions
 
-def input_fn(input_data):
-    raise NotImplementedError
 
-def output_fn(prediction, accept):
-    raise NotImplementedError
+def input_fn(input_data: bytes) -> Tuple[tuple, dict]:
+    args_kwargs_tuple = util.deserialize_sagemaker_input(input_data)
+    return args_kwargs_tuple
+
+
+def output_fn(predictions: Union[dict, tuple], accept: str) -> bytes:
+    """accept: can be ignored since the function from which this is called will handle it"""
+    predicition_bytes = util.serialize_sagemaker_output(predictions)
+    return predicition_bytes
