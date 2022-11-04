dca90387c0eb023cbeabb852f43439512f98122a
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-11-04 13:13:54 -0600

    add configurations

--- src/diffusion_util/diffusion_util/config.py
+++ src/diffusion_util/diffusion_util/config.py
@@ -1,7 +1,8 @@
 config = dict(
     model_repository="CompVis/stable-diffusion-v-1-4",
     huggingface_token_secret_name="huggingface_api_token",
-    model_bucket_name=None,
-    input_bucket_name=None,
-    output_bucket_name=None,
+    model_bucket_name=NotImplemented,
+    model_s3_key=NotImplemented,
+    input_bucket_name=NotImplemented,
+    output_bucket_name=NotImplemented,
 )
