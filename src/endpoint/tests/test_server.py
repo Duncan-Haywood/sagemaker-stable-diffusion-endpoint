75909da373ec1143ba6ce6e42d476b21ca8ed4df
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-11-16 14:03:45 -0700

    skip test server due to dependency issues

--- src/endpoint/tests/test_server.py
+++ src/endpoint/tests/test_server.py
@@ -2,7 +2,7 @@ from endpoint import server
 import pytest
 
 
-# @pytest.mark.skip(reason="no teardown")
+@pytest.mark.skip(reason="no teardown")
 def test_main():
     server.main()
     raise NotImplementedError
