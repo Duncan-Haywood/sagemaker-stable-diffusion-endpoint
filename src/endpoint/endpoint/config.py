a7759bcecdbb1599d1caafda723132260e3eb388
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-11-18 12:23:42 -0700

    remove unused configs

--- src/endpoint/endpoint/config.py
+++ src/endpoint/endpoint/config.py
@@ -1,9 +1,5 @@
 config = dict(
     model_repository="CompVis/stable-diffusion-v-1-4",
     huggingface_token_secret_name="huggingface_api_token",
-    model_bucket_name=NotImplemented,
-    model_s3_key=NotImplemented,
-    input_bucket_name=NotImplemented,
-    output_bucket_name=NotImplemented,
-    endpoint_name=NotImplemented,
+    model_s3_key="model",
 )
