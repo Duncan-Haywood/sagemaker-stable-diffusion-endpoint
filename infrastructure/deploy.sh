b0994c515b5b3d906c83b7fdc5e74c37365c6022
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-11-18 12:21:12 -0700

    documentation on deploy

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,3 +1,6 @@
+# push to github main branch before running this
+# create an administrator role with credentials for configure
+# run from the folder this file is in
 aws configure
 cdk bootstrap
 poetry run cdk synth
