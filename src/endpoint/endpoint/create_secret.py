3e76243b24ed054342b45adb3d59c148c2de3574
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-11-18 12:23:52 -0700

    logging

--- src/endpoint/endpoint/create_secret.py
+++ src/endpoint/endpoint/create_secret.py
@@ -1,13 +1,17 @@
 import typer
 from . import util
+from .logger import get_logger
+
+logger = get_logger(__name__)
 
 
-def main():
+def create_hugging_face_secret():
     token = typer.prompt("hugging face api token:")
     secret_name = util.get_huggingface_secret_name()
     description = "Huggingfce api token for model hub"
     util.create_secret(secret_name, token, description=description)
+    logger.info("created a hugging face secret with token")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    typer.run(create_hugging_face_secret)
