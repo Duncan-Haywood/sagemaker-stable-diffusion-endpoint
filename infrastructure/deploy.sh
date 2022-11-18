a7ea943ba84e553fa2ae76ac4dc3db829e9922ca
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-11-18 16:49:23 -0700

    deploy script

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,7 +1,8 @@
 # push to github main branch before running this
 # create an administrator role with credentials for configure
 # run from the folder this file is in
+poetry install
 aws configure
-cdk bootstrap
+poetry run cdk bootstrap
 poetry run cdk synth
 poetry run cdk deploy
