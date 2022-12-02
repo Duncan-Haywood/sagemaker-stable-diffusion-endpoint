b7ba8abb174df5d9569f2d9664924a30e65c8dcc
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-12-02 12:23:53 -0700

    bug fixes

--- infrastructure/infrastructure/devops.py
+++ infrastructure/infrastructure/devops.py
@@ -17,7 +17,17 @@ class PipelineStack(Stack):
         self.pipeline = pipelines.CodePipeline(
             self,
             "Pipeline",
-            synth=synth(source),
+            synth=pipelines.CodeBuildStep(
+                "Synth",
+                input=source,
+                commands=[
+                    "cd infrastructure",
+                    "pip install poetry",
+                    "poetry install",
+                    "npm install -g aws-cdk",
+                    "poetry run cdk synth --output ../cdk.out",
+                ],
+            ),
             code_build_defaults=pipelines.CodeBuildOptions(
                 build_environment=codebuild.BuildEnvironment(
                     compute_type=codebuild.ComputeType.MEDIUM,
@@ -32,7 +42,11 @@ class PipelineStack(Stack):
 
         self.pipeline.add_stage(
             EndpointStage(
-                self, "TestStage", production=False, image_uri=sagemaker_image_uri
+                self,
+                "TestStage",
+                production=False,
+                general_image_uri=general_image_uri,
+                sagemaker_image_uri=sagemaker_image_uri,
             ),
             pre=[unit_tests(general_image_uri)],
             post=[integration_tests(general_image_uri)],
@@ -40,76 +54,52 @@ class PipelineStack(Stack):
 
         self.pipeline.add_stage(
             EndpointStage(
-                self, "ProdStage", production=True, image_uri=sagemaker_image_uri
+                self,
+                "ProdStage",
+                production=True,
+                general_image_uri=general_image_uri,
+                sagemaker_image_uri=sagemaker_image_uri,
             ),
             pre=[pipelines.ManualApprovalStep("PromoteToProd")],
             post=[integration_tests(general_image_uri)],
         )
 
 
-class AssetStack(Stack):
-    def __init__(
-        self,
-        scope: Construct,
-        construct_id: str,
-        file_name="Dockerfile",
-        file_path="src/endpoint",
-        **kwargs,
-    ) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-        self.repo = ecr.Repository(self, "Repository")
-        image_repo_name = self.repo.repository_name
-        self.repository_uri = self.repo.repository_uri
-        pipelines.StackSteps(
-            stack=self,
-            post=[
-                upload_image(image_repo_name, self.repository_uri, file_name, file_path)
-            ],
-        )
-
-
 class AssetStage(Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        self.general_docker_assets = AssetStack(self, "GeneralDockerECR")
-        self.sagemaker_docker_assets = AssetStack(
-            self, "SagemakerEndpointDocker", file_name="Dockerfile.endpoint"
+        self.general_ecr = AssetStack(self, "GeneralECR")
+        self.sagemaker_ecr = AssetStack(
+            self, "SagemakerEndpointECR", file_name="Dockerfile.endpoint"
         )
-        # TODO the repo_uri might be different from the image uri, but probably works according to docs
-        self.sagemaker_image_uri = self.general_docker_assets.repository_uri
-        self.general_image_uri = self.sagemaker_docker_assets.repository_uri
+        self.sagemaker_image_uri = self.general_ecr.repository_uri
+        self.general_image_uri = self.sagemaker_ecr.repository_uri
 
 
 class EndpointStage(Stage):
     def __init__(
-        self, scope: Construct, construct_id: str, production: bool = False, **kwargs
+        self,
+        scope: Construct,
+        construct_id: str,
+        general_image_uri=None,
+        sagemaker_image_uri=None,
+        production: bool = False,
+        **kwargs,
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
         # create stacks
-        self.app = EndpointStack(self, "EndpointStack")
+        self.app = EndpointStack(self, "EndpointStack", image_uri=sagemaker_image_uri)
         # add post processing steps
         pipelines.StackSteps(
             stack=self.app,
             post=[
-                upload_model_step(self.app.model_bucket_name),
+                upload_model_step(general_image_uri, self.app.model_bucket_name),
                 set_endpoint_in_parameter_store(production, self.app.endpoint_name),
             ],
         )
 
 
 ## variables and functions referenced above
-def synth(source):
-    return pipelines.CodeBuildStep(
-        "Synth",
-        input=source,
-        commands=[
-            "cd infrastructure",
-            "pip install poetry",
-            "poetry install",
-            "npm install -g aws-cdk",
-            "poetry run cdk synth --output ../cdk.out",
-        ],
-    )
 
 
 def unit_tests(image_uri):
@@ -159,14 +149,17 @@ def set_endpoint_in_parameter_store(production, endpoint_name):
     return step
 
 
-def upload_model_step(model_bucket_name):
+def upload_model_step(image_uri, model_bucket_name):
     return pipelines.CodeBuildStep(
         "UploadModel",
         commands=[
-            "cd src/endpoint",
-            "docker build --tag endpoint .",
-            f"docker run --name endpoint -e 'model_bucket_name={model_bucket_name}' './endpoint/upload_model.py'",
+            "python ./endpoint/upload_model.py",
         ],
+        build_environment=codebuild.BuildEnvironment(
+            compute_type=codebuild.ComputeType.LARGE,
+            build_image=image_uri,
+        ),
+        env=dict(model_bucket_name=model_bucket_name),
     )
 
 
@@ -189,3 +182,24 @@ def upload_image(image_repo_name, repository_uri, file_name, file_path):
         ),
         cache=codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER),
     )
+
+
+class AssetStack(Stack):
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        file_name="Dockerfile",
+        file_path="src/endpoint",
+        **kwargs,
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.repo = ecr.Repository(self, "Repository")
+        image_repo_name = self.repo.repository_name
+        self.repository_uri = self.repo.repository_uri
+        pipelines.StackSteps(
+            stack=self,
+            post=[
+                upload_image(image_repo_name, self.repository_uri, file_name, file_path)
+            ],
+        )
