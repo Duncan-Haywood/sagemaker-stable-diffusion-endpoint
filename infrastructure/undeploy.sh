6aa056917546451cb3de84086228ef48bda91b96
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-12-05 16:10:24 -0700

    add dependencies to undeploy.sh

--- infrastructure/undeploy.sh
+++ infrastructure/undeploy.sh
@@ -1,5 +1,7 @@
 # you will also need to tear down resources that were bootstrapped and resources for the codepipeline. Google will give instructions.
 echo "configure"
 aws configure
+echo "install dependencies"
+poetry install
 echo "cdk destroy"
 cdk destroy --force
