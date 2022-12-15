14b1e05eb871d84db89d5a649b744e2fa96b60a6
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-12-15 13:31:49 -0700

    update server code

--- src/endpoint/endpoint/server.py
+++ src/endpoint/endpoint/server.py
@@ -5,7 +5,8 @@ from retrying import retry
 from subprocess import CalledProcessError
 from sagemaker_inference import model_server
 from .logger import get_logger
-
+import sys
+import shlex
 logger = get_logger(__name__)
 
 
@@ -18,16 +19,16 @@ def _start_mms():
     # by default the number of workers per model is 1, but we can configure it through the
     # environment variable below if desired.
     # os.environ['SAGEMAKER_MODEL_SERVER_WORKERS'] = '2'
-    model_server.start_model_server(
-        handler_service="/home/model-server/model_handler.py:handle"
-    )
+    model_server.start_model_server()
 
 
 def main():
-    # start server
-    _start_mms()
+    if sys.argv[1] == "serve":
+        _start_mms()
+        logger.info("server started")
+    else:
+        subprocess.check_call(shlex.split(" ".join(sys.argv[1:])))
     # prevent docker exit -- might not be needed in async version when shutting down constantly
-    logger.info("server started")
     subprocess.call(["tail", "-f", "/dev/null"])
 
 
