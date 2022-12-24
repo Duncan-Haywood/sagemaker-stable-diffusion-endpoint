98ec5ed0a92ef0849d5bd24dfc282455d509b97c
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-12-24 14:45:26 -0700

    try fixing model upload

--- src/upload_model/upload_model/upload_model.py
+++ src/upload_model/upload_model/upload_model.py
@@ -7,14 +7,11 @@ from .logger import get_logger
 logger = get_logger(__name__)
 
 
-def load_model(model_id, hugging_face_token):
+def load_model(hugging_face_token):
     """loads model from HuggingFace model hub into memory"""
     try:
         model = StableDiffusionInpaintPipeline.from_pretrained(
-            model_id,
-            use_auth_token=hugging_face_token,
-            revision="fp16",
-            torch_dtype=torch.float16,
+            "CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16
         )
         logger.info("model_download succeeded")
         return model
@@ -47,7 +44,6 @@ def model_exists(bucket_name, key) -> bool:
 def main():
     """Download model from hugging face model hub and upload to s3 bucket."""
     # config
-    model_id = "CompVis/stable-diffusion-v-1-4"
     local_dir = "./model"
     bucket_name = util.get_model_bucket_name()
     logger.info("bucket_name = %s" % bucket_name)
@@ -55,7 +51,7 @@ def main():
     hugging_face_token = util.get_hugging_face_token()
     # upload file
     if not model_exists(bucket_name, key):
-        model = load_model(model_id, hugging_face_token)
+        model = load_model(hugging_face_token)
         save_model_local(model, local_dir)
         util.upload_file_to_s3(bucket_name, local_dir, key)
         logger.info("model uploaded to s3")
