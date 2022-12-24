3a8a4f566d352bb22cfc681d9069aea1f9f63fd3
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-12-24 11:59:52 -0700

    add permissions

--- infrastructure/infrastructure/devops.py
+++ infrastructure/infrastructure/devops.py
@@ -194,5 +194,6 @@ def upload_model(model_bucket_name):
         env_from_cfn_outputs={"model_bucket_name": model_bucket_name},
         role_policy_statements=[
             iam.PolicyStatement(actions=["s3:*"], resources=["*"])
+            iam.PolicyStatement(actions=["secretsmanager:GetSecretValue"], resources=["*"])
         ],
     )
